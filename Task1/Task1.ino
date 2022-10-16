a9b74be9752ff43c45015608ad275987b9c0d438
MohamedSamerr <110998175+MohamedSamerr@users.noreply.github.com>
2022-10-17 01:50:30 +0200

    added codes

---
+++ Task1/Task1.ino
@@ -0,0 +1,96 @@
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include <ros.h>
+#include <std_msgs/Float64.h>
+
+ros::NodeHandle node;
+
+std_msgs::Float64 yaw;
+ros::Publisher IMU("IMU", &yaw);
+
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+MPU6050 mpu;
+
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+//==================================================================================
+
+void setup() {
+  
+    node.initNode();
+    node.advertise(IMU);
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+  
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) 
+    {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        mpu.CalibrateAccel(6);
+        mpu.CalibrateGyro(6);
+        mpu.PrintActiveOffsets();
+        mpu.setDMPEnabled(true);
+        mpuIntStatus = mpu.getIntStatus();
+        dmpReady = true;
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } 
+}
+
+//====================================================================================
+
+void loop() {
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+    // read a packet from FIFO
+    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            yaw.data = ypr[0] * 180/M_PI;
+            IMU.publish( &yaw);
+            node.spinOnce();
+       
+        #endif
+
+
+    }
+}
---
+++ Task2/Task2.ino
@@ -0,0 +1,57 @@
+#include <ros.h>
+#include <std_msgs/Int64.h>
+#define outputA PA6
+#define outputB PA7
+long long counter=0;
+
+ros::NodeHandle node;
+
+std_msgs::Int64 msg;
+
+ros::Publisher encoder("encoder", &msg);
+
+void setup() 
+{
+
+  
+  node.initNode();
+  node.advertise(encoder);
+  pinMode(outputA,INPUT_PULLUP);
+  pinMode(outputB,INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(outputA), interrupt_A , CHANGE);
+  attachInterrupt(digitalPinToInterrupt(outputB), interrupt_B , CHANGE);
+
+
+}
+
+void loop() 
+{
+  msg.data = counter ;
+  encoder.publish( &msg );
+  node.spinOnce();
+  delay(10);
+
+
+}
+void interrupt_A (void)
+{
+  if(digitalRead(outputA)!=digitalRead(outputB))
+  {
+    counter++;
+  }
+  else
+  {
+    counter--;
+  }
+}
+void interrupt_B (void)
+{
+  if(digitalRead(outputA)==digitalRead(outputB))
+  {
+    counter++;
+  }
+  else
+  {
+    counter--;
+  }
+}
